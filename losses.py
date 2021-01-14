8090d54fb8d276d2f98de9a68177e0e0319de475
yinguobing <yinguobing@gmail.com>
2021-01-14 18:35:07 +0800

    add margin loss

--- losses.py
+++ losses.py
@@ -1,31 +1,50 @@
 """This module provides the loss function of ArcFace"""
 import tensorflow as tf
-from tensorflow.python.ops import losses
+from math import pi
 
 
 class ArcLoss(tf.keras.losses.Loss):
-    """Additive angular margin loss."""
+    """Additive angular margin loss.
 
-    def __init__(self, num_ids, use_softmax=False, name="arcloss"):
-        """Build an additive angular margin loss object for Keras model.
+    Original implementation: https://github.com/deepinsight/insightface
+    """
 
-        Args:
-            num_ids: number of identities.
-            use_softmax: output softmax values for loss.
-        """
+    def __init__(self, margins=(1.0, 0.5, 0.0), scale=64, name="arcloss"):
+        """Build an additive angular margin loss object for Keras model."""
         super().__init__(name=name)
-        self.use_softmax = use_softmax
-        self.num_ids = num_ids
+        self.m1 = margins[0]
+        self.m2 = margins[1]
+        self.m3 = margins[2]
+        self.scale = scale
 
     def call(self, y_true, y_pred):
-        if self.use_softmax:
-            y_true = tf.one_hot(tf.cast(y_true, dtype=tf.int32),
-                                depth=self.num_ids,
-                                dtype=tf.float32)
-            
-            losses = tf.nn.softmax_cross_entropy_with_logits(y_true, y_pred)
+        mapping_label_onehot = y_true
+        fc7 = y_pred
+        
+        if self.m1 == 1.0 and self.m2 == 0.0:
+            _one_hot = mapping_label_onehot * self.m3
+            fc7 = fc7 - _one_hot
         else:
-            # TODO: implement arcloss.
-            pass
+            fc7_onehot = fc7 * mapping_label_onehot
+            cos_t = fc7_onehot
+            t = tf.math.arccos(cos_t)
 
-        return losses
+            if self.m1 != 1.0:
+                t = t * self.m1
+
+            if self.m2 != 0.0:
+                t = t + self.m2
+
+            margin_cos = tf.math.cos(t)
+            if self.m3 != 0.0:
+                margin_cos = margin_cos - self.m3
+
+            margin_fc7 = margin_cos
+            margin_fc7_onehot = margin_fc7 * mapping_label_onehot
+            diff = margin_fc7_onehot - fc7_onehot
+
+            fc7 = fc7 + diff
+
+        fc7 = fc7 * self.scale
+
+        return fc7
